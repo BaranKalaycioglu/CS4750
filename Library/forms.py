54f290acab02d7256c377612a1c83c48850ff060
BaranKalaycioglu <bk3xgp@virginia.edu>
2023-06-30 13:40:37 -0400

    modified:   Library/forms.py 	new file:   Library/templates/game_list.html 	modified:   Library/templates/landingPage.html

--- Library/forms.py
+++ Library/forms.py
@@ -6,7 +6,7 @@ class GameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ['name', 'genre', 'developer', 'platform', 'price']
-        labels = {'name': "Game Name", 'developer': 'Game Developer", 'platform': "Platform", 'price': "Game Price"}
+        labels = {'name': 'Game Name', 'developer': 'Game Developer', 'platform': 'Platform', 'price': 'Game Price'}
 
 class UserGameForm(forms.ModelForm):
     class Meta:
